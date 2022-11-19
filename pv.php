c9199c745fcadf3452d68b75604c273f7275a3cd
matteooooo <matteooooo@yahoo.it>
2022-11-19 08:30:27 +0100

    Modificato il refresh della pagina da 10 a 2 minuti

--- pv.php
+++ pv.php
@@ -5,7 +5,7 @@
 <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
 <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
 <script type="text/javascript" src="http://code.highcharts.com/highcharts.js"></script>
-<meta http-equiv="refresh" content="6000">
+<meta http-equiv="refresh" content="120">
 
 
 <!--link type='text/css' href='js/CookieCompliance/stylesheet.css' rel='stylesheet'-->
